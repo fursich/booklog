1c58660f0546a267ea6255530b4360a3ca93e7ad
Koji <fursich0@gmail.com>
2017-01-08 10:58:35 +0000

    password変更画面修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'https://elites-landing-page-fursich.c9users.io' }
+  config.action_mailer.default_url_options = { host: 'https://booklog-fursich.c9users.io' }
+
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
